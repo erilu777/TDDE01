48d607de6e72356f950a14ec135527a45cf8161a
Erik Lundqvist <erilu777@student.liu.se>
2024-12-11 10:17:10 +0100

    revision of lab 1 assn 2 done

--- Labs/lab1/Scripts/lab1_assn2.R
+++ Labs/lab1/Scripts/lab1_assn2.R
@@ -7,41 +7,47 @@ library(caret)
 ### TASK 1 ###
 
 # read the data
-data <- read.csv("../Data/parkinsons.csv")
+data <- read.csv("parkinsons.csv")
 
-# Get the number of rows in the dataset
-n <- dim(data)[1]
+voice_features <- c("Jitter...", "Jitter.Abs.", "Jitter.RAP", "Jitter.PPQ5", "Jitter.DDP",
+                    "Shimmer", "Shimmer.dB.", "Shimmer.APQ3", "Shimmer.APQ5", "Shimmer.APQ11", "Shimmer.DDA",
+                    "NHR", "HNR",
+                    "RPDE",
+                    "DFA",
+                    "PPE")
+
+model_data <- data[, c(voice_features, "motor_UPDRS")]
+
+# Get the number of rows in the data set
+n <- dim(model_data)[1]
 
 # set random seed for reproducibility
 set.seed(12345)
 
 # Partition 60% of data for the training set
 id <- sample(1:n, floor(n * 0.6))
-train <- data[id, ]
+train <- model_data[id, ]
 
 # Partition 40% of the data for the testing set
 id1 <- setdiff(1:n, id)
-test <- data[id1, ]
+test <- model_data[id1, ]
 
 # Scale training data with caret
-scaler <- preProcess(train)  # learns scaling from training data
-trainScaled <- predict(scaler, train)  # applies scaling to training data
+scaler <- preProcess(train)  # Learns scaling from training data
+trainScaled <- predict(scaler, train)  # Applies scaling to training data
 
 # Scale test data using same parameters from training data
 testScaled <- predict(scaler, test)
-
 
 ### TASK 2, Linear Regression Model ###
 
 # Fit linear regression model using scaled training data
 # motor_UPDRS is the target variable, use all other columns as predictors
-# motor_UPDRS means we're trying to predict the motor_UPDRS 
-# ~ . means we're using all other columns (.) as predictors
-# trainScaled is our training data
-fit <- lm(formula = motor_UPDRS ~ ., data=trainScaled)
+# motor_UPDRS means we're trying to predict motor_UPDRS 
+fit <- lm(formula = motor_UPDRS ~ . - 1, data=trainScaled)  # -1 removes intercept
 
 # Get summary to see which variables are significant
-summary(fit) # total_UPDRS has a p-value of 2e-16, making it the most significant
+summary(fit) # DFA has a t-value of -13.921, the predictor with the highest absolute t-value
 
 # Calculate MSE for training data
 pred_train <- predict(fit, trainScaled)  # get predictions for training data
@@ -55,14 +61,12 @@ mse_test <- mean((testScaled$motor_UPDRS - pred_test)^2)  # calculate test MSE
 print(paste("Training MSE:", round(mse_train, 4)))
 print(paste("Test MSE:", round(mse_test, 4)))
 
-
 ### TASK 3 ###
 
 # 3a #
 loglikelihood <- function(theta, sigma, traindata){
   # Get X matrix (predictors) from training data
-  # Add column of 1s for intercept
-  X <- cbind(1, as.matrix(traindata[, -which(names(traindata) == "motor_UPDRS")]))
+  X <- as.matrix(traindata[, -which(names(traindata) == "motor_UPDRS")])
   
   # Get y (actual values) from training data
   y <- traindata$motor_UPDRS
@@ -84,7 +88,7 @@ ridge <- function(theta, sigma, lambda, traindata){
   
   # Calculate ridge penalty using: lambda * sum(theta^2)
   # We exclude first theta (intercept) from penalty
-  ridge_penalty <- lambda * sum(theta[-1]^2)
+  ridge_penalty <- lambda * sum(theta^2)
   
   # Return total negative log-likelihood + ridge penalty
   return(neg_loglik + ridge_penalty)
@@ -93,8 +97,8 @@ ridge <- function(theta, sigma, lambda, traindata){
 # 3c #
 # Function that takes lambda and finds optimal theta and sigma
 ridgeOpt <- function(lambda, traindata) {
-  # Number of parameters we need (number of columns except motor_UPDRS)
-  n_params <- ncol(traindata) 
+  # Number of parameters we need (number of columns - 1 for motor_UPDRS)
+  n_params <- ncol(traindata) - 1  
   
   # Function that optim will minimize
   # Takes parameters and returns ridge value
@@ -117,68 +121,52 @@ ridgeOpt <- function(lambda, traindata) {
 }
 
 # 3d #
-# Higher lambda => lower DF (degree of freedom)
 DF <- function(lambda, traindata){
-  # Step 1: Create X matrix (same as before)
-  # Remove motor_UPDRS and add column of 1s
-  X <- cbind(1, as.matrix(traindata[, -which(names(traindata) == "motor_UPDRS")]))
+  # Remove motor_UPDRS
+  X <- as.matrix(traindata[, -which(names(traindata) == "motor_UPDRS")])
   
-  # Get how many parameters we have
-  p <- ncol(X)  # number of columns = number of parameters
-  
-  # For ridge regression, we calculate df using this formula:
-  # First: X'X (X transpose times X)
+  # Matrix Magic
+  p <- ncol(X)
   XtX <- t(X) %*% X
-  
-  # Add ridge part (lambda * I)
-  # diag(p) creates matrix with 1s on diagonal, 0s elsewhere
   ridge_matrix <- XtX + lambda * diag(p)
-  
-  # Calculate final df using trace (sum of diagonal)
   df <- sum(diag(X %*% solve(ridge_matrix) %*% t(X)))
-  
   return(df)
 }
 
-
 ### 4 ###
 
-# ridgeOpt returns the optimal parameters for a given lambda like the following:
-# result1[1]      # theta0 (intercept)
-# result1[2:22]   # theta1 to theta21 (coefficients for predictor variables)
-# result1[23]     # sigma (dispersion parameter)
+# ridgeOpt returns the optimal parameters using the ridge function
+
 result1 <- ridgeOpt(lambda=1, trainScaled)        # lambda=1
 result100 <- ridgeOpt(lambda=100, trainScaled)    # lambda=100
 result1000 <- ridgeOpt(lambda=1000, trainScaled)  # lambda=1000
 
 # Function to calculate MSE
 calculate_mse <- function(theta, sigma, data) {
-  # Make X matrix
-  X <- cbind(1, as.matrix(data[, -which(names(data) == "motor_UPDRS")]))
-  # Calculate predictions
-  pred <- X %*% theta
-  # Calculate MSE
+  # Make X matrix without intercept
+  X <- as.matrix(data[, -which(names(data) == "motor_UPDRS")])
+  pred <- X %*% theta # Calculate predictions
   mse <- mean((data$motor_UPDRS - pred)^2)
   return(mse)
 }
 
+#### Calculate MSE for each lambda ####
+
+# Calculate how many predictors we have
+n_params <- ncol(model_data) - 1  # -1 for motor_UPDRS
+
 # Calculate MSE for each lambda
 # Lambda = 1
-mse_train1 <- calculate_mse(result1[1:22], result1[23], trainScaled)
-mse_test1 <- calculate_mse(result1[1:22], result1[23], testScaled)
+mse_train1 <- calculate_mse(result1[1:n_params], result1[n_params + 1], trainScaled)
+mse_test1 <- calculate_mse(result1[1:n_params], result1[n_params + 1], testScaled)
 
 # Lambda = 100
-mse_train100 <- calculate_mse(result100[1:22], result100[23], trainScaled)
-mse_test100 <- calculate_mse(result100[1:22], result100[23], testScaled)
+mse_train100 <- calculate_mse(result100[1:n_params], result100[n_params + 1], trainScaled)
+mse_test100 <- calculate_mse(result100[1:n_params], result100[n_params + 1], testScaled)
 
 # Lambda = 1000
-mse_train1000 <- calculate_mse(result1000[1:22], result1000[23], trainScaled)
-mse_test1000 <- calculate_mse(result1000[1:22], result1000[23], testScaled)
-
-# Calcaulate degrees of freedom (DF) for each lambda
-df1 <- DF(1, trainScaled)
-df100 <- DF(100, trainScaled)
-df1000 <- DF(1000, trainScaled)
+mse_train1000 <- calculate_mse(result1000[1:n_params], result1000[n_params + 1], trainScaled)
+mse_test1000 <- calculate_mse(result1000[1:n_params], result1000[n_params + 1], testScaled)
 
 # Calculate degrees of freedom (DF) for each lambda
 df1 <- DF(1, trainScaled)
@@ -188,7 +176,7 @@ df1000 <- DF(1000, trainScaled)
 # Print the MSE results and degrees of freedom
 print(paste("Lambda = 0 (without ridge function):    Train MSE:", round(mse_train, 4), 
             "Test MSE:", round(mse_test, 4), 
-            "DF: 21"))  # Full df for linear regression = number of predictors
+            "DF:", n_params))  # DF is number of predictors (no intercept)
 print(paste("Lambda = 1:    Train MSE:", round(mse_train1, 4), 
             "Test MSE:", round(mse_test1, 4),
             "DF:", round(df1, 4)))
@@ -206,13 +194,14 @@ print(paste("Lambda = 1000: Train MSE:", round(mse_train1000, 4),
 # Data frame of coefficients for comparison
 coef_comparison <- data.frame(
   Variable = colnames(trainScaled)[-which(names(trainScaled) == "motor_UPDRS")],
-  Lambda0 = fit$coefficients[-1],  # Linear regression coefficients (excluding intercept)
-  Lambda1 = result1[2:22],         # Ridge coefficients for λ=1
-  Lambda100 = result100[2:22],     # Ridge coefficients for λ=100
-  Lambda1000 = result1000[2:22]    # Ridge coefficients for λ=1000
+  Lambda0 = fit$coefficients,  # All coefficients (no intercept to exclude)
+  Lambda1 = result1[1:n_params],
+  Lambda100 = result100[1:n_params],
+  Lambda1000 = result1000[1:n_params]
 )
 
 # Print to look at the coefficients
 print("Coefficient comparison:")
 print(coef_comparison)
+
 
