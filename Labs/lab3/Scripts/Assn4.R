87acaa7f895c1f7efc78075825465904ead7a57d
oliver <oliverstoltz123@gmail.com>
2024-12-17 17:28:43 +0100

    återigen nytt försök, uppgift 4 klar med tillhörade bilder

--- Labs/lab3/Scripts/Assn4.R
+++ Labs/lab3/Scripts/Assn4.R
@@ -1,5 +1,4 @@
 
-#### Install packages ####
 install.packages("neuralnet")
 library(neuralnet)
 
@@ -12,11 +11,12 @@ Var <- runif(500, 0, 10)
 mydata <- data.frame(Var, Sin=sin(Var))
 tr <- mydata[1:25,] # Training
 te <- mydata[26:500,] # Test
-
-nn_logi <- neuralnet(Sin ~ Var, tr, hidden = 10)
+# Random initialization of the weights in the interval [-1, 1]
+winit <- runif(10,-1,1)
+nn_logi <- neuralnet(Sin ~ Var, tr, hidden = 10, startweights = winit)
 
 # Plot of the training data (black), test data (blue), and predictions on the test data (red)
-plot(tr, cex=2)
+plot(tr, cex=2, main="Training Data, Test Data, and NN Predictions")
 points(te, col = "blue", cex=1)
 points(te[,1],predict(nn_logi,te), col="red", cex=1)
 legend("bottomleft", 
@@ -27,31 +27,33 @@ legend("bottomleft",
 
 #### Assignment 4.2 ####
 # Neural network with the activation function to be linear f(x) = x
-nn_linear <- neuralnet(Sin ~ Var, tr, hidden = 10, act.fct = function(x) x)
+nn_linear <- neuralnet(Sin ~ Var, tr, hidden = 10, act.fct = function(x) x, startweights = winit)
 
 #ReLU function
 ReLU <- function (x){
   ifelse(x >= 0, x, 0) #in the report explain why we did  not use return or pmax()
 }
 # Neural network with the activation function ReLU f(x)=x,  ifelse(x >= 0, x, 0)
-nn_ReLU <- neuralnet(Sin ~ Var, tr, hidden = 10, act.fct = ReLU)
+nn_ReLU <- neuralnet(Sin ~ Var, tr, hidden = 10, act.fct = ReLU, startweights = winit)
 
 #Softplus function
 softplus <- function(x){
   log(1+exp(x))
 }
 # Neural network with the activation function softplus f(x)= log(1+exp(x)
-nn_softplus <- neuralnet(Sin ~ Var, tr, hidden = 10, act.fct = softplus)
+nn_softplus <- neuralnet(Sin ~ Var, tr, hidden = 10, act.fct = softplus, startweights = winit)
 
-plot(tr, cex=0.7)
-points(te, col = "blue") # p
-points(te[,1],predict(nn_linear,te), col="red") # Can only learn linear relationships
-points(te[,1],predict(nn_ReLU,te), col="green")
-points(te[,1],predict(nn_softplus,te), col="orange")
+plot(tr, cex=2, ylim = c(-1, 1.5), main="Predictions of NN models with Linear, ReLU, and Softplus\n activation functions, compared to Training and Test data"
+)
+points(te, col = "blue") # Test data points
+points(te[,1], predict(nn_linear, te), col="red") # Linear model predictions
+points(te[,1], predict(nn_ReLU, te), col="green") # ReLU model predictions
+points(te[,1], predict(nn_softplus, te), col="orange") # Softplus model predictions
 legend("bottomleft", 
        legend = c("Training Data", "Test Data", "Linear", "ReLU", "Softplus"), 
        col = c("black", "blue", "red", "green", "orange"), 
        pch = 1, cex = 0.8)
+
 
 #### Assignment 4.3 ####
 
@@ -61,17 +63,17 @@ mydata2 <- data.frame(Var, Sin=sin(Var))
 test2 <- mydata2[1:500,]
 
 # Set up the plot with x-axis limits between 0 and 50
-plot(tr, cex = 1, xlim = c(0, 50), ylim = c(-10, 1.5), main = "Sine Function Predictions", xlab = "Var", ylab = "Sin(Var)")
+plot(tr, cex = 1, xlim = c(0, 50), ylim = c(-12, 1.5), main = "Sine Function Predictions", xlab = "Var", ylab = "Sin(Var)")
 
 # Add test data points in blue
 points(test2, col = "blue", cex = 1)
 
 # Add predictions from the neural network in red
-points(test2[, 1], predict(nn_logi, test2), col = "red")
-abline(h = -9.5, col = "green", lwd = 2)
+points(test2[, 1], predict(nn_logi, test2), col = "red", cex=1)
+abline(h = -9, col = "green", lwd = 2)
 
 legend("bottomleft", legend = c("Training Data", "Test Data", "NN Predictions 
-on test data", "sin(var) = - 9.5"), 
+on test data", "sin(var) = - 9"), 
        col = c("black", "blue", "red", "green"), pch = c(1, 1, 1), cex = 0.8)
 
 #### Assignment 4.4 ####
@@ -81,7 +83,6 @@ print(nn_logi$weights)
 # Look at the weights
 
 #### Assignment 4.5 ####
-
 
 # Sample points
 set.seed(1234567890)  # Reset seed for reproducibility
@@ -109,11 +110,3 @@ legend("bottomleft",
        col = c("blue", "red"),
        pch = 1,
        cex = 0.8)
-
-
-
-
-
-
-
-
